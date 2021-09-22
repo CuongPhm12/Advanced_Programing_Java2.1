697a45d811f0da586d5c74b80a2f7daa7ebab4d8
unknown <phamvietcuong12@gmail.com>
2021-09-22 11:28:13 +0700

    xay dung lơp chi ghi trong Java

---
+++ src/xayDungLopChiGhiTrongJava/Student.java
@@ -0,0 +1,17 @@
+package xayDungLopChiGhiTrongJava;
+
+public class Student {
+    private String name = "John";
+    private String classes = "C02";
+
+    public Student() {
+    }
+
+     void setName(String name) {
+        this.name = name;
+    }
+
+     void setClasses(String classes) {
+        this.classes = classes;
+    }
+}
---
+++ src/xayDungLopChiGhiTrongJava/Test.java
@@ -0,0 +1,9 @@
+package xayDungLopChiGhiTrongJava;
+
+public class Test {
+    public static void main(String[] args) {
+        Student student = new Student();
+        student.setName("Mai Trang");
+        student.setClasses("C0821H1");
+    }
+}
